dd05864ec50b013c4791be61aa26c4a5de3bfd5b
ashutosh2329 <sonutiwari2329@gmail.com>
2020-05-26 12:59:33 +0530

    changes

--- staircase.py
+++ staircase.py
@@ -1,20 +1,20 @@
-# import math
-# import os
-# import random
-# import re
-# import sys
+import math
+import os
+import random
+import re
+import sys
 
-# # Complete the staircase function below.
-# def staircase(n):
-#     for i in range(n):
-#         for j in reversed(range(n)):
-#             if(j-1>=i):
-#                 print(" ", end = '')
-#             else:
-#                 print("#", end = '')
-#         print()
+# Complete the staircase function below.
+def staircase(n):
+    for i in range(n):
+        for j in reversed(range(n)):
+            if(j-1>=i):
+                print(" ", end = '')
+            else:
+                print("#", end = '')
+        print()
     
-# if __name__ == '__main__':
-#     n = int(input())
+if __name__ == '__main__':
+    n = int(input())
 
-#     staircase(n)
+    staircase(n)
